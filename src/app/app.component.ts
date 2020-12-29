703ae99ab5cc932d10f729ffbea3de82d70c97c4
Laura Spieker <laura.spieker@nerdery.com>
2020-12-29 16:43:19 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+function log (target, name, descriptor) {
+  console.log(target, name, descriptor)
+  const original = descriptor.value
+
+  descriptor.value = function() {
+    console.log("this function was hacked")
+  }
+  return descriptor
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +17,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'intro2angular';
+
+  constructor() {
+    this.aSimpleMethod()
+  }
+
+  @log
+  aSimpleMethod() {
+    console.log("hey there")
+  }
 }
